5cc0d47bb492b29fd98dda88415f4f1e1be46b32
Jamil Filho <jamilfilho@outlook.com.br>
2021-01-25 11:44:25 -0300

    chore: redirects
    redirecting the structure of old links to the current structure

--- next.config.js
+++ next.config.js
@@ -1,5 +1,24 @@
 module.exports = {
     publicRuntimeConfig: {
       FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID
-    }
+    },
+    async redirects() {
+      return [
+        {
+          source: '/2021/um-ensaio-sobre-a-marca-da-besta/',
+          destination: '/um-ensaio-sobre-a-marca-da-besta/',
+          permanent: true,
+        },
+        {
+          source: '/2020/editorial/bem-vindoa-ao-teoleigo/',
+          destination: '/bem-vindoa-ao-teoleigo/',
+          permanent: true,
+        },
+        {
+          source: '/2020/literatura-e-cristianismo/depravacao-total-graca-e-misericordia-em-robinson-crusoe/',
+          destination: '/literatura-e-cristianismo/depravacao-total-graca-e-misericordia-em-robinson-crusoe/',
+          permanent: true,
+        }
+      ]
+    },
 }
