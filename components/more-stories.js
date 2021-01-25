89eac0afd9c5be59e68507fc08404ced92f75c62
Jamil Filho <jamilfilho@outlook.com.br>
2021-01-25 16:21:01 -0300

    fix: fix padding on home page

--- components/more-stories.js
+++ components/more-stories.js
@@ -2,7 +2,7 @@ import PostPreview from '../components/post-preview'
 
 export default function MoreStories({ posts }) {
   return (
-    <section>
+    <section className="w-10/12">
       <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
         {posts.map((post) => (
           <PostPreview
