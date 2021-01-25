11836608a76208a80a3f002a954f72304704f377
Jamil Filho <jamilfilho@outlook.com.br>
2021-01-25 18:15:20 -0300

    fix: comments
    updating facebook id

--- components/comments.js
+++ components/comments.js
@@ -5,7 +5,7 @@ export default function ArticleComments({ url }) {
     return(
         <div className="mt-20 pt-12 pb-10 bg-gray-100">
             <div className="px-5 lg:flex lg:justify-center lg:min-w-medium">
-                <FacebookProvider appId={process.env.FACEBOOK_APP_ID} className="lg:min-w-medium" width="100%">
+                <FacebookProvider appId="255605512589652" className="lg:min-w-medium" width="100%">
                     <Comments href={`${CMS_URL}/${url}`} />
                 </FacebookProvider>
             </div>
