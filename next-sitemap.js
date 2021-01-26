1e82944c7b4c45e325e799d391ffa4d270f46a11
Jamil Filho <jamilfilho@outlook.com.br>
2021-01-25 21:01:14 -0300

    feat: adding sitemap support

---
+++ next-sitemap.js
@@ -0,0 +1,10 @@
+module.exports = {
+    siteUrl: 'https://teoleigo.com.br',
+    generateRobotsTxt: true,
+    // optional
+    robotsTxtOptions: {
+      additionalSitemaps: [
+        'https://teoleigo.com.br/sitemap.xml',
+      ],
+    },
+  }
